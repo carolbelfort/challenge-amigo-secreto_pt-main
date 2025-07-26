e89cea26d74b71de0bf717d53c99f574b45962e7
Ana Carolina Belfort <caaarolbelfort@gmail.com>
2025-07-26 19:07:01 -0300

    Implementa função de sorteio de amigo

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,9 +1,9 @@
  //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let listaDeAmigos = [];
+const listaDeAmigos = [];
 
 function adicionarAmigo() {
-    let input = document.getElementById("nome-amigo");
-    let nome = input.value.trim();
+    const campo = document.getElementById("nome-amigo");
+    const nome = input.value.trim();
 
     if (nome === "") {
         alert("Por favor, insira um nome.");
@@ -11,7 +11,32 @@ function adicionarAmigo() {
     }
 
     listaDeAmigos.push(nome);
-    input.value = "";
+    campo.value = "";
+    exibirLista();
 }
 
+function exibirLista() {
+    const lista = document.getElementById("lista-amigos");
+    lista.innerHTML = "";
 
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        const item = document.createElement("li");
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
+
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("A lista de amigos está vazia.");
+        return;
+    }
+
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceSorteado];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `Amigo sorteado: <strong>${amigoSorteado}</strong>`;
+    
+}
